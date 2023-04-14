58725f5648f153e3e5eea284cb9913f545a43df6
Husam Obeidat <husamobe1991@gmail.com>
2023-04-14 21:18:26 +0000

    starting the server

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # back_end_GameZone
+
+# back_end_GameZone
+
+## installing required framework and dependencies ##
+
+<code>npm init -y<code><br>
+<code>npm install express<code><br>
+<code>npm install axios<code><br>
+<code>npm install cors<code><br>
+<code>npm install nodemon<code><br>
+<code>npm install pg<code><br>
+
+
+-------------------------------------
+
+## 1- Building the Server.
+<ul>
+<li> Make the server listen to port</li>
+<li> Creating the Home Route '/'</li>
+<li> The homeHandler function get the data from an API using GET method</li>
+<li> Create a GAMES constructor to shape the data </li>
+<li> Map through data using map method and return "title, thumbnail, description, genre, game_url, release_date"</li>
+</ul>
+
+-------------------------------------------
+
+## 2- Building The Database.
+<ul>
+<li>install database postgres "npm install pg"</</li>
+<li>create database "movie_data"</li>
+<li>create schema.sql file and inside create table games</li>
+<li>connect the database with schema.sql using this command "psql  -d games_data -f schema.sql"</li>
+</ul>
---
+++ server.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+require('dotenv').config();
+const app = express();
+app.use(cors());
+const port = process.env.PORT;
+const API_URL = process.env.API_URL;
+
+//    Routes
+app.get('/', homeHandler);// get all games data
+
+
+//creating a constructor for Games
+function Games(title, thumbnail, description, genre, game_url, release_date) {
+    this.title = title;
+    this.thumbnail = thumbnail;
+    this.description = description;
+    this.genre = genre;
+    this.game_url = game_url;
+    this.release_date = release_date;
+
+}
+
+
+function homeHandler(req, res) {
+    axios.get(API_URL)
+        .then((result) => {
+            let shapedData = result.data.map((game) => {
+                return new Games(game.title, game.thumbnail, game.short_description, game.genre, game.game_url, game.release_date)
+            })
+            res.json(shapedData);
+        })
+        .catch((err) => { console.log(err); })
+}
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+})
