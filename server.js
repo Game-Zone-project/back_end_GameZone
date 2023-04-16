47739e9538160a56c6c0ef9d0dd62fa39c34f1ac
muhammad <moalsad001@gmail.com>
2023-04-16 00:18:06 +0000

    added new Routes in backend almost finished git add .

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
@@ -16,11 +17,26 @@ const API_URL = process.env.API_URL;
 app.use(express.json());
 
 
-//    Routes
-//   http://localhost:3000/
+//Routes
+//http://localhost:3000/
 app.get('/', homeHandler);
-//   http://localhost:3000/addGame
+//http://localhost:3000/getAllGame
+app.get('/getAllGame', getAllGamesHandlers);
+//http://localhost:3000/addGame
 app.post('/addGame', addGameHandler);
+//http://localhost:3000/DELETE/5
+app.delete('/DELETE/:id', deleteGameHandler);
+//http://localhost:3000/updateGames/4
+app.put('/updateGames/:id', updateGamesHandler);
+
+
+//handleserver error
+//app.use(handleServerError);
+
+//should be the last line in routes   //// * => mean any thing than your routes links!!
+app.get('*', handleNotFoundError);
+
+
 
 
 //creating a constructor for Games
@@ -42,8 +58,21 @@ function homeHandler(req, res) {
                 return new Games(game.title, game.thumbnail, game.short_description, game.genre, game.game_url, game.release_date)
             })
             res.json(shapedData);
-        })
-        .catch((err) => { console.log(err); })
+        }).catch()
+    // .catch((error)=>{
+    //     handleServerError(error,req, res);
+    // })
+}
+
+function getAllGamesHandlers(req, res) {
+    let sql = `SELECT * FROM games;`
+    client.query(sql).then((result) => {
+        //console.log(result.rows);
+        res.json(result.rows);
+    }).catch()
+    // .catch((error)=>{
+    //     handleServerError(error,req, res);
+    // })
 }
 
 function addGameHandler(req, res) {
@@ -53,9 +82,48 @@ function addGameHandler(req, res) {
     let values = [title, genre, image, review, rating, release_date, game_URL];
     client.query(sql, values).then(result => {
         res.status(201).json(result.rows);
-    })
+    }).catch()
+    // .catch((error)=>{
+    //     handleServerError(error,req, res);
+    // })
 }
 
+
+
+function deleteGameHandler(req, res) {
+    let [id] = req.params.id;
+    let sql = `DELETE FROM games WHERE id = $1;`;
+    let value = [id];
+    client.query(sql, value).then(result => {
+        res.status(204).send("deleted");
+    }).catch()
+    // .catch((error)=>{
+    //     handleServerError(error,req, res);
+    // })
+}
+
+function updateGamesHandler(req, res){
+    let [id] = req.params.id
+    let { review } = req.body
+    let sql = "UPDATE games SET review=$1 WHERE id=$2 RETURNING *;";
+    let values = [review, id];
+    console.log(values)
+    client.query(sql, values)
+        .then(result => {
+            res.send(result.rows)
+        }).catch()
+}
+
+//500 not found 
+// function handleServerError(err,req, res) {
+//     res.status(500).send({status:500,responseText:"Sorry, something went wrong"});
+//}
+//404 not found error
+
+function handleNotFoundError(req, res) {
+    console.log("hi");
+    res.status(404).send("Not Found !");
+}
 
 client.connect()
     .then(() => {
